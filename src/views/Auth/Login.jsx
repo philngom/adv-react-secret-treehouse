07c24f49e357505463fe308cc0d38baeb727c31d
Philippe Ngom <philngom@gmail.com>
2022-05-06 04:00:36 -0700

    handlelogin function complete. form values added and tracked in state

--- src/views/Auth/Login.jsx
+++ src/views/Auth/Login.jsx
@@ -13,11 +13,9 @@ export default function Login() {
 
   // The `from` property of `location.state` gives us
   // the URL to redirect to after logging in.
-  const { from } = location.state || { from: { pathname: '/' } };
+  const { from } = location.state || { from: { pathname: '/treehouse' } };
 
   const handleLogin = (event) => {
-    event.preventDefault();
-    const loginWasSuccessful = auth.login(formState.email, formState.password);
 
     // TODO: If login was unsuccessful, set an error with a message
     // to display to the user that their login failed.
@@ -26,23 +24,37 @@ export default function Login() {
     // from React Router to replace the current URL with the URL
     // we need to redirect to.
     // See https://v5.reactrouter.com/web/api/history for the appropriate method to use
+    try {
+      event.preventDefault();
+      console.log('inhere');
+      const loginWasSuccessful = auth.login(formState.email, formState.password);
+      console.log("🚀 ~ file: Login.jsx ~ line 31 ~ handleLogin ~ formState.email, formState.password", formState.email, formState.password)
+      console.log("🚀 ~ file: Login.jsx ~ line 31 ~ handleLogin ~ loginWasSuccessful", loginWasSuccessful)
+
+      history.replace(from);
+
+    } catch (error) {
+      setError('Login Failed. Try again.')
+    }
   };
 
   return (
     <>
       <h3>You must log in to view the page at {from.pathname}</h3>
-      <form onSubmit={handleLogin} className={styles.loginForm}>
+      <form onSubmit={(event) => handleLogin(event)} className={styles.loginForm} onChange={ handleFormChange }>
         <label>Email</label>
         <input
           id="email"
           name="email"
           type="email"
+          value={ formState.email }
         />{' '}
         <label>Password</label>
         <input
           id="password"
           name="password"
           type="password"
+          value={ formState.password }
         />
         <button type="submit" aria-label="Sign In">
           Sign in
